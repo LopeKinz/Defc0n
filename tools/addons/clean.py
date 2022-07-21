c769803355bf165375c7aced96f9c122c8d391bf
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2022-07-21 23:10:06 +0200

    Add files via upload

---
+++ tools/addons/clean.py
@@ -0,0 +1,8 @@
+# Import modules
+import os
+
+# Clear command line
+if os.name == "nt":
+    os.system("@cls & @title Defc0n ToolKit & @color e")
+else:
+    os.system("clear")
---
+++ tools/addons/logo.py
@@ -0,0 +1,13 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.MAGENTA}
+
+    ___        __         ___         
+   /   \ ___  / _|  ___  / _ \  _ __  
+  / /\ // _ \| |_  / __|| | | || '_ \ 
+ / /_//|  __/|  _|| (__ | |_| || | | |
+/___,'  \___||_|   \___| \___/ |_| |_|
+From Limerboy. Remastered by Pinkyhax
+{Fore.RESET}""")
